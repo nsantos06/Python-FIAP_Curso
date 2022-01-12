444be774a93f91f8dba0ff7c5e0881f66a2a6998
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-01-12 18:47:25 -0300

    Create Variaveis2.py

---
+++ Capitulo2-Variaveis/Variaveis2.py
@@ -0,0 +1,6 @@
+responsavel = input("Digite o nome do responsável: ")
+funcionario = input("Digite o nome do funcionáiro: ")
+evento = input("Digite o nome do evento: ")
+valor = float(input("Digite o valor da entrada: "))
+
+print("Declaro para o senhor " + responsavel + ", que o senhor " + funcionario + " esteve presente no evento " + evento + " e gastou o valor em RS de: " + str(valor) + " com a entrada." )
