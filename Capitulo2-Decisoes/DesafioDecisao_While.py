8e79e5e4aed79f87448fb6f847d3d117f1e9e0b4
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-01-12 18:49:53 -0300

    Create DesafioDecisao_While.py

---
+++ Capitulo2-Decisoes/DesafioDecisao_While.py
@@ -0,0 +1,22 @@
+resposta = "SIM"
+while resposta == "SIM":
+    nivel = input("Digite o seu nivel:").upper()
+    if nivel == "ADM" or "USR":
+        genero = input("Digite o genero:").upper()
+        if nivel == "ADM":
+            if genero == "FEMININO":
+                print("Olá Administradora!")
+            else:
+                print("Olá Administrador!")
+
+        else:
+            if genero == "FEMININO":
+                print("Olá usuaria!")
+            else:
+                print("Olá usuario!")
+
+    elif nivel == "GST":
+        print("Ola visitante!")
+    else:
+        print("olá desconhecido(a)!")
+    resposta = input("Digite SIM para continuar").upper()
