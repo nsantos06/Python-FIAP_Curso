90522c84d30646fd3f44c54489cf41545505c4c2
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-01-14 19:09:56 -0300

    Create Dicionarios1.py

---
+++ Capitulo4_Dicionarios/Dicionarios1.py
@@ -0,0 +1,9 @@
+usuarios = {}
+usuarios = {
+    "Chaves":["Chaves Silva", "17/06/2017", "Recep_01"],
+    "Quico":["Enrico Flores", "03/06/2017", "Raiox_02"]
+}
+
+print(usuarios)
+print("\n")
+print("Dados:", usuarios.get("Chaves"))
