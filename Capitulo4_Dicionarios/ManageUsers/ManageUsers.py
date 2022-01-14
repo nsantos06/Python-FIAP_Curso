fd21ca6b2d74e0af30f8c9a07d40752e65092401
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-01-14 19:11:28 -0300

    Create ManageUsers.py

---
+++ Capitulo4_Dicionarios/ManageUsers/ManageUsers.py
@@ -0,0 +1,9 @@
+from Capitulo4_Dicionarios.Funcoes import *
+
+usuarios = {}
+opcao = perguntar()
+
+while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L":
+    if opcao == "I":
+       inserir(usuarios)
+    opcao = perguntar()
