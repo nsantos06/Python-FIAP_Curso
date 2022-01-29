5604a73c5c1f8586e57fc2c82fea49731c024784
nsantos06 <51927883+nsantos06@users.noreply.github.com>
2022-01-29 13:44:35 -0300

    Create Inventario.py

---
+++ 3_3_ManipulaArquivos/Inventario.py
@@ -0,0 +1,22 @@
+from Funcoes.Funcoes_Arquivos import *
+inventario = {}
+
+opcao = chamarMenu()
+
+while opcao>0 and opcao <4:
+    if opcao == 1:
+        registrar(inventario)
+    elif opcao == 2:
+        persistir(inventario)
+    elif opcao == 3:
+        resultado = exibir()
+        for linha in resultado:
+            separacao = linha[linha.find(";") + 1:-1]
+            data = separacao[0:separacao.find(";")]
+            separacao = separacao[separacao.find(";") + 1:-1]
+            descricao = separacao[0:separacao.find(";")]
+            departamento = linha[linha.rfind(";") + 1:-1]
+            print("Data.........: ", data)
+            print("Descrição....: ", descricao)
+            print("Departamento.: ", departamento)
+    opcao = chamarMenu()
